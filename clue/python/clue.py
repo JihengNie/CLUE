034a494ed74f470cccf376289885500e06d75a15
Jiheng Nie <Niejiheng@gmail.com>
2023-02-06 17:58:27 +0000

    Added in interface from command line:

--- clue/python/clue.py
+++ clue/python/clue.py
@@ -35,19 +35,22 @@ cards = {
 
 # Players set up
 
-# playerNum = int(input("Enter number of players: "))
-# players = []
+playerNum = int(input("Enter number of players: "))
+players = set()
 
-# for i in range(playerNum):
-#   playerName = input("Enter player " + str(i + 1) + "'s name: " )
-#   players.append(playerName)
+for i in range(playerNum):
+  playerName = input("Enter player " + str(i + 1) + "'s name: " )
+  players.add(playerName)
+
+print('Players:', players)
 
 # Functions
-def revealing_cards(cardType, cardName, player):
+def revealingCards(cardType, cardName, playerName, cards):
   cards[cardType][cardName]["revealed"] = True
-  cards[cardType][cardName]["owned"] = player
+  cards[cardType][cardName]["owned"] = playerName
+  return displayOwnedCards(playerName, cards)
 
-def playerCardsOwned(playerName, cards):
+def displayOwnedCards(playerName, cards):
   cardsOwned = []
   for cardTypes in cards:
     for items in cards[cardTypes]:
@@ -55,5 +58,55 @@ def playerCardsOwned(playerName, cards):
         cardsOwned.append(items)
   return cardsOwned
 
-print(playerCardsOwned("Jade", cards))
-print(playerCardsOwned(None, cards))
+def displayRevealedCardsByType(cardType, cards):
+  revealedType = []
+  for items in cards[cardType]:
+    if cards[cardType][items]["revealed"] == False:
+      revealedType.append(items)
+  return revealedType
+
+def displayAllPlayerOwnedCards(players, cards):
+  playerOwnedCards = {}
+  for names in players:
+    playerOwnedCards[names] = displayOwnedCards(names, cards)
+  return playerOwnedCards
+
+# Playing the game
+gameStatus = True
+roundCounter = 0
+
+while gameStatus:
+  print("\nRound's Passed: " + str(roundCounter))
+  action = input("Choose an action: \n  1. Reveal a card \n  2. Displaying remaining cards \n  3. End Game \n")
+  remainingCards = {
+    "characters": displayRevealedCardsByType('characters', cards),
+    "weapons" : displayRevealedCardsByType('weapons', cards),
+    "rooms" : displayRevealedCardsByType('rooms', cards)
+  }
+  if action == "1":
+    cardType = input("Choose a card type: characters, weapons, or rooms: ")
+    if cardType == 'characters':
+      print(remainingCards["characters"])
+      cardName = input("Choose a character: ")
+    elif cardType == 'weapons':
+      print(remainingCards["weapons"])
+      cardName = input("Choose a weapon: ")
+    elif cardType == 'rooms':
+      print(remainingCards["rooms"])
+      cardName = input("Choose a room: ")
+    else:
+      break
+    print(players)
+    playerName = input("Enter player name: ")
+    revealingCards(cardType, cardName, playerName, cards)
+    roundCounter += 1
+  elif action == "2":
+    print("characters", remainingCards["characters"])
+    print("weapons", remainingCards["weapons"])
+    print("rooms", remainingCards["rooms"])
+    roundCounter += 1
+  elif action == "3":
+    gameStatus = False
+  else:
+    print("Try again")
+  print(displayAllPlayerOwnedCards(players, cards))
