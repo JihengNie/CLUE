44bd39bb0b628c02686c361d1583e6a49a9e8dde
Jiheng Nie <Niejiheng@gmail.com>
2023-02-15 05:04:59 +0000

    Fixing enterRoundGuess function

--- clue/python/cluev2.py
+++ clue/python/cluev2.py
@@ -34,9 +34,9 @@ cards = {
 }
 
 players = {
-  "player0": {"potentialCards":["Dagger", "Ball Room"], "notOwned": ["Study", "Lounge"] },
-  "player1": {"potentialCards":["Dagger", "Ball Room"], "notOwned": ["Study", "Lounge"] },
-  "player2": {"potentialCards":["Dagger", "Ball Room"], "notOwned": ["Study", "Lounge"] }
+  "player0": {"potentialCards":[], "notOwned": [] },
+  "player1": {"potentialCards":[], "notOwned": [] },
+  "player2": {"potentialCards":[], "notOwned": [] }
 }
 
 rounds = [
@@ -81,34 +81,34 @@ def initializationPlayers(players):
 def enterPlayerHand(players, cards, selectionObject):
   selectedCardsList = []
 
-  print(selectionObject["players"])
+  print("Players:", selectionObject["players"])
   selectedPlayer = int(input("Enter the number for the player: "))
 
-  print(selectionObject["characters"])
-  selectedCharacters = input("Enter the number assoicated with the character cards you own seprated by commas: ").replace(" ", "")
+  print("Character Cards:", selectionObject["characters"])
+  selectedCharacters = input("Enter the number assoicated with the character cards seprated by commas: ").replace(" ", "")
   if selectedCharacters != "":
     charactersList = convertStrToInt(selectedCharacters.split(","))
     for items in charactersList:
       selectedCardsList.append(selectionObject["characters"][items])
       del selectionObject["characters"][items]
 
-  print(selectionObject["weapons"])
-  selectedWeapons = input("Enter the number assoicated with the weapons cards you own seprated by commas: ")
+  print("Weapons Cards:", selectionObject["weapons"])
+  selectedWeapons = input("Enter the number assoicated with the weapons cards seprated by commas: ")
   if selectedWeapons != "":
     weaponsList = convertStrToInt(selectedWeapons.split(","))
     for items in charactersList:
       selectedCardsList.append(selectionObject["weapons"][items])
       del selectionObject["weapons"][items]
 
-  print(selectionObject["rooms"])
-  selectedRooms = input("Enter the number assoicated with the rooms cards you own seprated by commas: ")
+  print("Room Cards:", selectionObject["rooms"])
+  selectedRooms = input("Enter the number assoicated with the rooms cards seprated by commas: ")
   if selectedRooms != "":
     roomsList = convertStrToInt(selectedRooms.split(","))
     for items in charactersList:
       selectedCardsList.append(selectionObject["rooms"][items])
       del selectionObject["rooms"][items]
 
-  print(selectedCardsList)
+  print("\nSelected cards:", selectedCardsList)
   for cardTypes in cards:
     for card in cards[cardTypes]:
         if card in selectedCardsList:
@@ -124,9 +124,94 @@ def viewPlayerOwnedCards(players, cards):
         if cards[cardTypes][card]["owned"] == player:
           currentHand.append(card)
     playerHand[player] = currentHand
+  print("PlayerHand:", playerHand)
   return playerHand
 
+def enterRoundGuess(selectionObject, players, rounds):
+  ### --------------------Problem: People can guess cards that is already known. Solution: Give user an option to select a None card-----------------------------------###
+  print("Please enter the guess.\n")
+  # Card Selection
+  print(selectionObject["characters"])
+  selectedCharacter = int(input("Enter the number for the character: "))
+
+  print(selectionObject["weapons"])
+  selectedWeapon = int(input("Enter the number for the weapon: "))
+
+  print(selectionObject["rooms"])
+  selectedRoom = int(input("Enter the number for the room: "))
+  cardSelected = [
+    selectionObject["characters"][selectedCharacter],
+    selectionObject["weapons"][selectedWeapon],
+    selectionObject["rooms"][selectedRoom],
+  ]
+  # Round guess
+  roundGuess = {
+    "whoAnswered": None,
+    "cards": cardSelected
+  }
+
+  stillGuessing = True
+  while stillGuessing:
+    guessCheck = input("Are you still guessing? y/n: ")
+    if guessCheck == "y":
+      # Player selection
+      print(selectionObject["players"])
+      selectedPlayerNum = int(input("Enter the number for the player: "))
+      selecterPlayer = selectionObject["players"][selectedPlayerNum]
+
+      shownCard = input("Did the player show a card? y/n: ")
+      if shownCard == "y":
+        roundGuess["whoAnswered"] = selecterPlayer
+        for card in cardSelected:
+          if card not in players[selecterPlayer]["potentialCards"]:
+            players[selecterPlayer]["potentialCards"].append(card)
+      else:
+        for card in cardSelected:
+          if card not in players[selecterPlayer]["notOwned"]:
+            players[selecterPlayer]["notOwned"].append(card)
+    else:
+      stillGuessing = False
+  rounds.append(roundGuess)
+  return players
+
+def displayingRemainingCards(selectionObject):
+  print("Characters:", selectionObject["characters"])
+  print("Weapons:", selectionObject["weapons"])
+  print("Rooms:", selectionObject["rooms"])
+
+# "player0": {"potentialCards":["Dagger", "Ball Room"], "notOwned": ["Study", "Lounge"] },
+
 # Test space
-enterPlayerHand(players, cards, selectionObject)
+# enterPlayerHand(players, cards, selectionObject)
 # print(selectionObject)
-print(viewPlayerOwnedCards(players, cards))
+# print(viewPlayerOwnedCards(players, cards))
+# print(enterRoundGuess(selectionObject, players, rounds))
+
+# Playing the game
+
+gameStatus = True
+
+while gameStatus:
+  inputMenu = """\nChoose an action:
+  1. Enter player hand
+  2. Enter a card player owns
+  3. Displaying player cards
+  4. Enter guesses
+  5. Display remaining cards
+  0. End Game
+    """
+  action = input(inputMenu)
+  if action == "0":
+    gameStatus = False
+  elif action == "1":
+    enterPlayerHand(players, cards, selectionObject)
+  elif action == "2":
+    enterPlayerHand(players, cards, selectionObject)
+  elif action == "3":
+    viewPlayerOwnedCards(players, cards)
+  elif action == "4":
+    enterRoundGuess(selectionObject, players, rounds)
+  elif action =="5":
+    displayingRemainingCards(selectionObject)
+  else:
+    print("Try again")
